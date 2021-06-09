f2c1c838df9a0a508c35b6dd293847a498c40ade
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2021-06-09 19:57:58 -0400

    Create python-dictionaires.py

---
+++ python-dictionaires.py
@@ -0,0 +1,8 @@
+dishes = ["pizza", "sauerkraut", "paella". "Hamburger"]
+contries = ["Italy", "Germany", "Spain", "USA"]
+
+country_specs = dict(zip(countires,dishes))
+
+print(country_specs.values())
+print('France' in country_specs)
+
